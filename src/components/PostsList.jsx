87807ac7c260e526069f270fd950cf359e199d65
Christian van lierop <you@example.com>
2024-05-16 16:31:52 +0200

    Started with lesson 28

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from './PostsList.module.css';
 import { useState } from 'react';
 
 export default function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredAuthor, setEnteredAuthor] = useState('Christian');
   const [enteredBody, setEnteredBody] = useState('The greatest on the mic');
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -16,9 +21,14 @@ export default function PostsList() {
   }
   return (
     <>
-      <Modal>
-        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
-      </Modal>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author='Manuel' body='Check out the full course' />
