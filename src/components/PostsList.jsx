985fd957ad78ece24bdf4af901496a07dda0276d
Christian van Lierop <chrisvanlierop@gmail.com>
2024-05-20 11:04:00 +0200

    Finished lesson 36

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,12 +4,29 @@ import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
