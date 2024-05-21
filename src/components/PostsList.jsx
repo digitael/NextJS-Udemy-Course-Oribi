94820b57b9a2f038adea925eb3910811f594a188
Christian van Lierop <chrisvanlierop@gmail.com>
2024-05-21 22:35:16 +0200

    Finished lesson 45

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,26 +6,12 @@ import classes from './PostsList.module.css';
 export default function PostsList() {
   const posts = useLoaderData();
 
-  function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
-
-  console.log('**********************');
-  console.log(posts);
-
   return (
     <>
       {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
-            <Post key={post.body} author={post.author} body={post.body} />
+            <Post key={post.id} id={post.id} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,40 +1,19 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, redirect, Form } from 'react-router-dom';
 
 import classes from './NewPost.module.css';
 import Modal from '../components/Modal';
 
-function NewPost({ onAddPost }) {
-  const [enteredAuthor, setEnteredAuthor] = useState('Christian');
-  const [enteredBody, setEnteredBody] = useState('The greatest on the mic');
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  function submitHandler(event) {
-    event.preventDefault();
-    const postData = {
-      body: enteredBody,
-      author: enteredAuthor,
-    };
-    onAddPost(postData);
-    onCancel();
-  }
-
+function NewPost() {
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <Form method='post' className={classes.form}>
         <p>
           <label htmlFor='body'>Text</label>
-          <textarea id='body' required rows={3} onChange={bodyChangeHandler} />
+          <textarea id='body' name='body' required rows={3} />
         </p>
         <p>
           <label htmlFor='name'>Your name</label>
-          <input type='text' id='name' required onChange={authorChangeHandler} />
+          <input type='text' id='name' name='author' required />
         </p>
         <p className={classes.actions}>
           <Link to='..' type='button'>
@@ -42,9 +21,23 @@ function NewPost({ onAddPost }) {
           </Link>
           <button>Submit</button>
         </p>
-      </form>
+      </Form>
     </Modal>
   );
 }
 
 export default NewPost;
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData);
+  await fetch('http://localhost:8080/posts', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return redirect('/');
+}
